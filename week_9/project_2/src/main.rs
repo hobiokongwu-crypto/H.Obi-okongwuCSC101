ebb03e45a34ea3b3d942ac4541d89a0fed79cbea
unknown <hobiokongwu@gmail.com>
2025-11-30 22:40:12 +0100

    My PAU-SMIS Project_2

--- week_9/project_2/src/main.rs
+++ week_9/project_2/src/main.rs
@@ -1,3 +1,28 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "Student_details.txt";
+
+    let students = vec![
+    (1, "Adekunle,J.", "PAU/CSC/19/001","CSC", 400),
+    (2, "Owen,C.", "PAU/INF/20/123","INF", 300),
+    (3, "Muhammad,A.", "PAU/ACC/21/456","ACC", 200),
+    (4, "Chijoke,A.", "PAU/FIN/22/789","FIN", 100),];
+
+    let mut output_content = String::new();
+    
+    let header = format!("{:.<5}{:<20}{:.<15}{:.<10}{:.<5}\n", "S/N", "Name", "Matric No.", "Dept", "Level");
+    println!("{}", header.trim_end());
+    output_content.push_str(&header);
+    output_content.push_str("----------------------------------------\n");
+
+    for student in students.iter(){
+        let line = format!("{:.<5}{:<20}{:.<15}{:.<10}{:.<5}\n", student.0, student.1, student.2, student.3, student.4);
+        println!("{}",line.trim_end());
+        output_content.push_str(&line);
+    }
+    let mut file = File::create(filename).expect("ERR:Failed to create the file!!!");
+    file.write_all(output_content.as_bytes()).expect("ERR:Failed to write the file!!!");
+    println!("\n👌Report saved to '{}'🎆", filename);
 }
