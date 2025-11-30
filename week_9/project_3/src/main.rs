207a3a09d32c8ed078c8a717c37df9cfdcf2cf08
unknown <hobiokongwu@gmail.com>
2025-11-30 23:10:06 +0100

    The EFCC convicted minister dataset

--- week_9/project_3/src/main.rs
+++ week_9/project_3/src/main.rs
@@ -1,3 +1,27 @@
+use std::fs::File;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "commissioner_list.txt";
+
+    let commissioners = ["Abiodun Alamutu Dauda", "Mustafa Azeem Bendu", "Okoroacha Titus Ogbona", "Adewale Jimoh Balogun", "Oluwole Faith Ezeh"];
+    let zones = ["South West", "North East", "South South", "South West", "South East"];
+    let ministries = ["Internal Affairs", "Justice", "Defense", "Power & Steel", "Petroleum"];
+    let mut output_content = String::new();
+    let header = format!("{:.<5}{:.<30}{:.<15}{:.<20}\n", "S/N", "COMMISSIONER", "ZONE", "MINISTRY");
+    println!("\n--THE MINISTRIAL COMMISSION LIST--");
+    println!("{}", header.trim_end());
+    output_content.push_str(&header);
+    output_content.push_str("--------------------------------------------------\n");
+
+
+    for i in 0..commissioners.len() {
+        let sn = i +1;
+        let line = format!("{:.<5}{:.<30}{:.<15}{:.<20}\n", sn, commissioners[i], zones[i], ministries[i]);
+        println!("{}", line.trim_end());
+        output_content.push_str(&line);
+    }
+    let mut file = File::create(filename).expect("ERR:Failed to create the file!!");
+    file.write_all(output_content.as_bytes()).expect("ERR:Failed to write the file!!");
+    println!("\n🎉The Report Shall Be Saved As '{}'🎊", filename);
 }
